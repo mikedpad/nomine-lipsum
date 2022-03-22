5d90bd6b42dad7689617f99be51013ee00440710
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2022-03-22 07:15:19 -0400

    Random "given" func removes array duplication

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,56 @@ import {
   surname as surnameNames,
 } from './data/names.json';
 
-const givenNames = [...maleNames, ...femaleNames, ...unisexNames];
+const allGivenLength = maleNames.length + femaleNames.length + unisexNames.length;
 
-function get(names: string[]): string {
-  return names[Math.floor(Math.random() * names.length)];
+/**
+ * Get a random name from the top 1000 male names in the US.
+ * Data provided by the US Social Security Administration.
+ * @returns {string} A random male name.
+ */
+export function male(): string {
+  return maleNames[Math.floor(Math.random() * maleNames.length)];
+}
+
+/**
+ * Get a random name from the top 1000 female names in the US.
+ * Data provided by the US Social Security Administration.
+ * @returns {string} A random female name.
+ */
+export function female(): string {
+  return femaleNames[Math.floor(Math.random() * femaleNames.length)];
+}
+
+/**
+ * Get a random name from the top 1000 unisex names in the US.
+ * Data provided by the US Social Security Administration.
+ * @returns {string} A random unisex name.
+ */
+export function unisex(): string {
+  return unisexNames[Math.floor(Math.random() * unisexNames.length)];
+}
+
+/**
+ * Get a random surname from the top 1000 surnames in the US.
+ * Data provided by the US Census Bureau.
+ * @returns {string} A random surname.
+ */
+export function surname(): string {
+  return surnameNames[Math.floor(Math.random() * surnameNames.length)];
+}
+
+/**
+ * Get a random first name from the top 1000 of the male, female and unisex categories in the US.
+ * Data provided by the US Social Security Administration.
+ * @returns {string} A random first name.
+ */
+export function given(): string {
+  const r = Math.floor(Math.random() * allGivenLength);
+  return r < maleNames.length
+    ? maleNames[r]
+    : r < maleNames.length + femaleNames.length
+    ? femaleNames[r - maleNames.length]
+    : unisexNames[r - maleNames.length - femaleNames.length];
 }
 
 /**
@@ -19,56 +65,11 @@ export function full(): string {
   return `${given()} ${surname()}`;
 }
 
-/**
- * Get a random first name from the top 1000 of the male, female and unisex categories in the US.
- * Data provided by the US Social Security Administration.
- * @returns {string} A random first name.
- */
-export function given(): string {
-  return get(givenNames);
-}
-
-/**
- * Get a random name from the top 1000 male names in the US.
- * Data provided by the US Social Security Administration.
- * @returns {string} A random male name.
- */
-export function male(): string {
-  return get(maleNames);
-}
-
-/**
- * Get a random name from the top 1000 female names in the US.
- * Data provided by the US Social Security Administration.
- * @returns {string} A random female name.
- */
-export function female(): string {
-  return get(femaleNames);
-}
-
-/**
- * Get a random name from the top 1000 unisex names in the US.
- * Data provided by the US Social Security Administration.
- * @returns {string} A random unisex name.
- */
-export function unisex(): string {
-  return get(unisexNames);
-}
-
-/**
- * Get a random surname from the top 1000 surnames in the US.
- * Data provided by the US Census Bureau.
- * @returns {string} A random surname.
- */
-export function surname(): string {
-  return get(surnameNames);
-}
-
 export default {
-  full,
-  given,
-  surname,
   male,
   female,
   unisex,
+  surname,
+  given,
+  full,
 };
